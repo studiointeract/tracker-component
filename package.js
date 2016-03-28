9eee6dc98bdc23d49cc6829ebde0d67e02a5ef48
Tim Brandin <tim.brandin@gmail.com>
2016-03-28 13:06:35 +0200

    Version bump.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'studiointeract:tracker-component',
-  version: '1.3.2',
+  version: '1.3.3',
   summary: 'Easy reactive React Components with Meteor and Tracker',
   git: 'https://github.com/studiointeract/tracker-component',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3-rc.1');
+  api.versionsFrom('1.3');
   api.use('ecmascript');
   api.use('tracker');
   api.use('jsx@0.2.4');
